fda9a9ae067b2a38925ac2b7890b7c7ad7f8ba4f
joe walters <joewalters999@gmail.com>
2021-11-16 22:56:09 +0000

    Schedule Helper and Get next X in schedule

---
+++ Patterns/Helpers/ContextHelpers/ScheduleHelper.cs
@@ -0,0 +1,28 @@
+﻿using NCrontab;
+using System;
+using System.Collections.Generic;
+
+namespace TNDStudios.ContextHelpers
+{
+    public class ScheduleHelper
+    {
+        private readonly string _cron;
+
+        public ScheduleHelper(string cron)
+        {
+            _cron = cron;
+        }
+
+        public DateTime GetNext()
+        {
+            var schedule = CrontabSchedule.Parse(_cron);
+            return schedule.GetNextOccurrence(DateTime.Now);
+        }
+
+        public IEnumerable<DateTime> GetNextX(DateTime end)
+        {
+            var schedule = CrontabSchedule.Parse(_cron);
+            return schedule.GetNextOccurrences(DateTime.Now, end);
+        }
+    }
+}
